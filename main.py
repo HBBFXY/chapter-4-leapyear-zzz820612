7fb55cf57aabacef0c06c4f6519844c127914f66
zzz820612 <zyz198312@qq.com>
2025-09-27 21:43:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# 在此处编写代码
+try:
+    year = int(input("请输入年份："))
+    if year%4 ==0:
+        if year % 400 == 0:
+            print("是闰年")
+        elif year % 100 != 0:
+            print("是闰年")
+        else:
+            print("不是闰年")
+    else:
+        print("不是闰年")
+except ValueError:
+    print("输入错误")# 在此处编写代码
